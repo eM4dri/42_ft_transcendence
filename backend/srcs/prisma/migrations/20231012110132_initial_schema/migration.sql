8f27f8ae054814c6007ef5df3626b3323c4cd940
emadriga <emadriga@student.42madrid.com>
2023-10-12 13:04:53 +0200

    [EMM]Cleaning migrations

--- backend/srcs/prisma/migrations/20231012110132_initial_schema/migration.sql
+++ backend/srcs/prisma/migrations/20231012110132_initial_schema/migration.sql
@@ -15,9 +15,46 @@ CREATE TABLE "users" (
 );
 
 -- CreateTable
+CREATE TABLE "channels" (
+    "channelId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "channelName" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "password" TEXT,
+
+    CONSTRAINT "channels_pkey" PRIMARY KEY ("channelId")
+);
+
+-- CreateTable
+CREATE TABLE "channel_users" (
+    "channelUserId" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isOwner" BOOLEAN NOT NULL DEFAULT false,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "isBanned" BOOLEAN NOT NULL DEFAULT false,
+    "mutedUntill" TIMESTAMP(3),
+    "leaveAt" TIMESTAMP(3),
+
+    CONSTRAINT "channel_users_pkey" PRIMARY KEY ("channelUserId")
+);
+
+-- CreateTable
+CREATE TABLE "channel_messages" (
+    "channelMessageId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "message" TEXT NOT NULL,
+    "channelUserId" TEXT NOT NULL,
+
+    CONSTRAINT "channel_messages_pkey" PRIMARY KEY ("channelMessageId")
+);
+
+-- CreateTable
 CREATE TABLE "chats" (
     "chatId" TEXT NOT NULL,
-    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "chats_pkey" PRIMARY KEY ("chatId")
 );
@@ -45,6 +82,7 @@ CREATE TABLE "chat_user_messages" (
 -- CreateTable
 CREATE TABLE "stats_user" (
     "userId" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
     "gamesWin" INTEGER NOT NULL DEFAULT 0,
     "gamesLose" INTEGER NOT NULL DEFAULT 0,
     "gamesDraw" INTEGER NOT NULL DEFAULT 0,
@@ -71,6 +109,24 @@ CREATE UNIQUE INDEX "users_url_key" ON "users"("url");
 -- CreateIndex
 CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "users_userId_login_key" ON "users"("userId", "login");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "channels_channelName_key" ON "channels"("channelName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "channel_users_channelId_userId_leaveAt_key" ON "channel_users"("channelId", "userId", "leaveAt");
+
+-- AddForeignKey
+ALTER TABLE "channel_users" ADD CONSTRAINT "channel_users_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "channels"("channelId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "channel_users" ADD CONSTRAINT "channel_users_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "channel_messages" ADD CONSTRAINT "channel_messages_channelUserId_fkey" FOREIGN KEY ("channelUserId") REFERENCES "channel_users"("channelUserId") ON DELETE CASCADE ON UPDATE CASCADE;
+
 -- AddForeignKey
 ALTER TABLE "chat_users" ADD CONSTRAINT "chat_users_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "chats"("chatId") ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -81,4 +137,4 @@ ALTER TABLE "chat_users" ADD CONSTRAINT "chat_users_userId_fkey" FOREIGN KEY ("u
 ALTER TABLE "chat_user_messages" ADD CONSTRAINT "chat_user_messages_chatUserId_fkey" FOREIGN KEY ("chatUserId") REFERENCES "chat_users"("chatUserId") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "stats_user" ADD CONSTRAINT "stats_user_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "stats_user" ADD CONSTRAINT "stats_user_userId_login_fkey" FOREIGN KEY ("userId", "login") REFERENCES "users"("userId", "login") ON DELETE CASCADE ON UPDATE CASCADE;
