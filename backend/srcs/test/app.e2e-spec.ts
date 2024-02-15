3fc90da3702e58c93d385c01d4e0cd1a6dffee2b
em4dri <edu.mail.delivery+github@gmail.com>
2024-02-15 13:40:46 +0100

    [EMM]e2e test chat finished

--- backend/srcs/test/app.e2e-spec.ts
+++ backend/srcs/test/app.e2e-spec.ts
@@ -3,8 +3,8 @@ import { Test } from "@nestjs/testing";
 import * as pactum from "pactum";
 import { AppModule } from "../src/app.module";
 import { PrismaService } from "../src/prisma/prisma.service";
-import { CreateUserDto, PatchUserDto } from "src/user/dto";
-import { CreateChatMessageDto } from "src/chat/dto";
+import { CreateUserDto } from "src/user/dto";
+
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -127,11 +127,11 @@ describe('App e2e', () => {
           })
           .expectStatus(200)
           .expect(ctx => {
-            expect(ctx.res.json[0]).toHaveProperty('username','login');
-            expect(ctx.res.json[0]).toHaveProperty('email','login@mail.com');
-            expect(ctx.res.json[1]).toHaveProperty('username', dto.username);
-            expect(ctx.res.json[1]).toHaveProperty('email', dto.email);
-            expect(ctx.res.json[1]).toHaveProperty('url', dto.url);
+            expect(ctx.res.json).toHaveProperty('response[0].username','login');
+            expect(ctx.res.json).toHaveProperty('response[0].email','login@mail.com');
+            expect(ctx.res.json).toHaveProperty('response[1].username', dto.username);
+            expect(ctx.res.json).toHaveProperty('response[1].email', dto.email);
+            expect(ctx.res.json).toHaveProperty('response[1].url', dto.url);
           });
       });
     });
@@ -229,14 +229,19 @@ describe('App e2e', () => {
   });
 
   describe('Chat', () => {
-
     const chatMessageDto = {
       listenerId: '$S{userId}',
       message: "Hi! this is the FIRST message",
     };
-    let chatId: string;
-    let chatMessageId: string;
-    let chatMessage2Id: string;
+    const chatId: string= '$S{chatId}';
+    const editMessage1Dto = {
+      chatMessageId: '$S{chatMessage1Id}',
+      message: "Hi! this is the FIRST message edited",
+    }
+    const editMessage2Dto = {
+      chatMessageId: '$S{chatMessage2Id}',
+      message: "Hi! this is the SECOND message edited",
+    }
     describe('Post chat message', () => {
       it('Should be created new chat with a message',() => {
         return pactum
@@ -247,14 +252,12 @@ describe('App e2e', () => {
           })
           .withBody(chatMessageDto)
           .stores('chatId','response.chatId')
-          .stores('chatMessageId','chatMessageId')
+          .stores('chatMessage1Id','response.message.chatMessageId')
           .expectStatus(201)          
           .expect(ctx => {
             expect(ctx.res.json).toHaveProperty('response.message.message', chatMessageDto.message);
           });
       });
-      chatId = '$S{chatId}';
-      chatMessageId = '$S{chatMessageId}';
       it('Should be created a new message in previous chat',() => {
         return pactum
           .spec()
@@ -266,12 +269,11 @@ describe('App e2e', () => {
             ...chatMessageDto,
             chatId
           })
-          .stores('chatMessage2Id','chatMessageId')
+          .stores('chatMessage2Id','response.message.chatMessageId')
           .expectStatus(201)
           .expectJsonLike('response.chatId', chatId)
           .expectJsonLike('response.message.message', chatMessageDto.message);
       });
-      chatMessage2Id = '$S{chatMessage2Id}';
       it('Shouldn\'t be created a new message unkown chatId',() => {
         return pactum
           .spec()
@@ -300,7 +302,7 @@ describe('App e2e', () => {
         });
     });
     describe('Get chat', () => {
-      it('Should get all chats',() => {
+      it('Should get all chat messages',() => {
         return pactum
           .spec()
           .get(`/chat/${chatId}`)
@@ -312,56 +314,89 @@ describe('App e2e', () => {
           .expectJsonLike('[1].message', chatMessageDto.message)
           .expectJsonLike('[0].chatId', chatId)
           .expectJsonLike('[1].chatId', chatId)
-        });
+      });
+      it('Should get no messages an unexitant chatId',() => {
+        return pactum
+          .spec()
+          .get(`/chat/00000000-0000-0000-0000-000000000000`)
+          .withHeaders({
+            Authorization: 'Bearer $S{accessToken}'
+          })
+          .expectStatus(200)
+          .expectBody([])
+      });
     });
     describe('Edit chat message', () => {
-      // it('Should be created new chat with a message',() => {
-      //   return pactum
-      //     .spec()
-      //     .put(`/chat/message`)
-      //     .withHeaders({
-      //       Authorization: 'Bearer $S{accessToken}'
-      //     })
-      //     .withBody({
-      //       chatMessageId,
-      //       message: "Hi! this is the FIRST message edited",
-      //     })
-      //     .expectStatus(201)          
-      //     .expect(ctx => {
-      //       expect(ctx.res.json).toHaveProperty('response.message.message', chatMessageDto.message);
-      //     });
-      // });
-      // it('Should be created a new message in previous chat',() => {
-      //   return pactum
-      //     .spec()
-      //     .put(`/chat/message`)
-      //     .withHeaders({
-      //       Authorization: 'Bearer $S{accessToken}'
-      //     })
-      //     .withBody({
-      //       chatMessageId: chatMessage2Id,
-      //       message: "Hi! this is the SECOND message edited",
-      //     })
-      //     .stores('chatMessage2Id','chatMessageId')
-      //     .expectStatus(201)
-      //     .expectJsonLike('response.chatId', chatId)
-      //     .expectJsonLike('response.message.message', chatMessageDto.message);
-      // });
-      // it('Shouldn\'t be created a new message unkown chatId',() => {
-      //   return pactum
-      //     .spec()
-      //     .put(`/chat/message`)
-      //     .withHeaders({
-      //       Authorization: 'Bearer $S{accessToken}'
-      //     })
-      //     .withBody({
-      //       ...chatMessageDto,
-      //       chatId: '00000000-0000-0000-0000-000000000000'
-      //     })
-      //     .expectStatus(404);
-      // });
+      it('Should be edit 1st chat message',() => {
+        return pactum
+          .spec()
+          .put(`/chat/message`)
+          .withHeaders({
+            Authorization: 'Bearer $S{accessToken}'
+          })
+          .withBody({
+            ...editMessage1Dto
+          })
+          .expectStatus(200)
+          .expectJsonLike('chatMessageId', editMessage1Dto.chatMessageId)
+          .expectJsonLike('message', editMessage1Dto.message)
+      });
+      it('Should be edit 2nd chat message',() => {
+        return pactum
+          .spec()
+          .put(`/chat/message`)
+          .withHeaders({
+            Authorization: 'Bearer $S{accessToken}'
+          })
+          .withBody({
+            ...editMessage2Dto
+          })
+          .expectStatus(200)
+          .expectJsonLike('chatMessageId', editMessage2Dto.chatMessageId)
+          .expectJsonLike('message', editMessage2Dto.message)
+      });
+      it('Shouldn\'t be edit unkown chat message',() => {
+        return pactum
+          .spec()
+          .put(`/chat/message`)
+          .withHeaders({
+            Authorization: 'Bearer $S{accessToken}'
+          })
+          .withBody({
+            chatMessageId: '00000000-0000-0000-0000-000000000000',
+            message: "Unkown chatMessageId should not be edited",
+          })
+          .expectStatus(404);
+      });
     });
-    describe('Delete chat message', () => {});
-   });
+    describe('Delete chat message', () => {
+      it('Should be delete 1st chat message',() => {
+        return pactum
+          .spec()
+          .delete(`/chat/message/${editMessage1Dto.chatMessageId}`)
+          .withHeaders({
+            Authorization: 'Bearer $S{accessToken}'
+          })
+          .expectStatus(200)
+          .expectJsonLike('chatMessageId', editMessage1Dto.chatMessageId)
+          .expectJsonLike('message', editMessage1Dto.message)
+      });
+      it('Shouldn\'t be delete 1st chat message twice',() => {
+        return pactum
+          .spec()
+          .delete(`/chat/message/${editMessage1Dto.chatMessageId}`)
+          .withHeaders({
+            Authorization: 'Bearer $S{accessToken}'
+          })
+          .expectStatus(404)
+          .expectJson({
+            "response": "Not Found"
+          })
+      });      
+    });
+  });
+
+  
+
 
 });
